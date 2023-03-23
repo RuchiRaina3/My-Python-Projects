b4953296af1a16a0c5507e5500472beef2b32aa7
Ruchi Raina <92518496+RuchiRaina3@users.noreply.github.com>
2023-03-23 15:52:16 +0530

    Update AutomaticFolderCleaner.py

--- AutomaticFolderCleaner.py
+++ AutomaticFolderCleaner.py
@@ -7,14 +7,6 @@ def makeDir(folder, parent=''):
         pass
 
 def folderList(files, file_extensions):
-    # os.path.splitext(filePath) returns (fileName, extension)
-    # print(files)
-    # print(file_extensions)
-    # for file in files:
-    #     print(os.path.splitext(file)[1])
-    #     if os.path.splitext(file)[1].lower() in file_extensions and os.path.isfile(file):
-    #         print(os.path.splitext(file))
-    # print("*************************")
     return [file for file in files if os.path.splitext(file)[1].lower() in file_extensions]
 
 def moveFiles(files, folder, p=''):
@@ -29,7 +21,6 @@ if __name__ == "__main__":
     all_files = os.listdir()
     all_files.remove('AutomaticFolderCleaner.py')
     folders = ['Images', 'Documents', 'Music', 'Videos', 'Applications', 'Zipped']
-    #Call makeDir() for all folders
     [makeDir(folder) for folder in folders]
 
     file_extensions = {'Images': ['.jpg', '.jpeg', '.png', '.gif', '.webp'], 'Documents': ['.doc', '.docx', '.txt',
@@ -38,11 +29,11 @@ if __name__ == "__main__":
                        'Videos': ['.mp4', '.mov', '.wmv', '.avi', ',mpeg-2'],
                        'Applications': ['.app', '.applictaion', '.exe'], 'Zipped': ['.zip'] }
 
-    # Make dictionary with key="Images" and value=file_extensions['Images']
     files_list = {k:folderList(all_files,file_extensions[k]) for k in folders}
 
     [moveFiles(files_list[folder],folder) for folder in folders]
 
+    #Make difefrent folders within document folders
     parent = 'Documents'
     document_files = os.listdir(parent)
     document_folders = ['Word', 'Presentation', 'Pdf', 'Text', 'Excel', 'CSV', 'Web Pages']
