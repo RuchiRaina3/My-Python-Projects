fd0f605ff356219813ea6eff653cd49555bb66f5
Ruchi Raina <92518496+RuchiRaina3@users.noreply.github.com>
2023-03-25 00:02:21 +0530

    Update Code.py

--- Projects/6 Degrees Of Separation/Code.py
+++ Projects/6 Degrees Of Separation/Code.py
@@ -4,28 +4,11 @@ Created on Thu Dec 10 14:30:59 2020
 
 @author: lockd
 
-We have facebook data in file facebook_combined.txt.gz. 
-This is in edge list representation. Here anonymous people 
-are taken as nodes and edge between any two nodes 
-represents friendship between them. Now, we have to prove
-the theory of Six Degrees of Separation.
-
-We will create graph by reading  all the nodes and edges 
-from edgelist using in-built function nx.read_edgelist()
-
-We have to find the length of shortest path(no. of edges 
-between the shortest path) between each pair of nodes. For
-that we will create a list 'N' containing all nodes of 
-graph and wll also create an empty list 'SPLL' for storing the 
-length
-
-Then, run a loop to find length l of shortest path between
-each pair of node and append this l to SPLL for each 
-pair of node
-
-Find minimum, maximum and average value of SPLL. We will 
-come to know that average value of SPLL is 6 i.e. on avg.
-length of shortest path between any pair of node is 6
+We will create graph by reading  all the nodes and edges from edgelist using in-built function nx.read_edgelist().
+We have to find the length of shortest path(no. of edges between the shortest path) between each pair of nodes. For that we will create a list 'N' containing all nodes of 
+graph and wll also create an empty list 'SPLL' for storing the length. Then, run a loop to find length l of shortest path between each pair of node and append this l to SPLL for each 
+pair of node.
+Find minimum, maximum and average value of SPLL. We will come to know that average value of SPLL is 6 i.e. on avg. length of shortest path between any pair of node is 6
 Hence, proving Six Degree Of Separation
 
 """
